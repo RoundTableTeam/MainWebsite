920720a8b2f7a68a13badb02cc7c8715fac856fb
Arian <itsarianebrahimi@gmail.com>
2021-10-29 23:39:38 +0330

    Init Commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<main>
+
+
+	<article>
+		<h1>Coming Soon ... 😎</h1>
+		<p>Under Maintenance👷</p>
+	</article>
+	<footer>
+		<h5>© The Round Table  - 2020-2021</h5>
+	</footer>
+</main>
+
+<style>
+html {
+    position: relative;
+    min-height: 100%;
+}
+
+article{
+	text-align: center;
+	padding: 2%;
+}
+article > p {
+	padding: 1%;
+}
+
+footer {
+	text-align: left;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    overflow: hidden;
+	padding: 1%;
+}
+
+
+</style>
